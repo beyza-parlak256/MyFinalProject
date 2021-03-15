b1dcaeeff251ac920ab41ecde357bdd7bbbf7035
Beyza Parlak <beyza.parlak98@erzurum.edu.tr>
2021-03-15 20:59:32 +0300

    8. ders sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -13,6 +13,7 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
+
             foreach (var product in productManager.GetByUnitPrice(40,100))
             {
                 Console.WriteLine(product.ProductName);
