85b25e48a287b2707a0a8b2541004baf25da81a1
Beyza Parlak <beyza.parlak98@erzurum.edu.tr>
2021-03-21 16:03:32 +0300

    9. gün sonu

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal _categorDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categorDal = categoryDal;
+        }
+
+        public List<Category> GetAll()
+        {
+            // İş kodları
+            return _categorDal.GetAll();
+        }
+
+        // Select * from Categories where CategoryId = 3
+        public Category GetById(int categoryId)
+        {
+            return _categorDal.Get(c => c.CategoryId == categoryId);
+        }
+    }
+}
--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -1,4 +1,6 @@
-﻿using Entities.Concrete;
+﻿using Core.DataAcces;
+using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,5 +10,8 @@ namespace DataAccess.Abstract
     // Interface Product Data Access Layer anlamına gelir - Veri tabanında yapılacak operasyonları içerir
     public interface IProductDal : IEntityRepository<Product>
     {
+        List<ProductDetailDto> GetProductDetails();
     }
 }
+
+// Code Refactoring
